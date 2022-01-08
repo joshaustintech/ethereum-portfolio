c385fcb4e9382b6737d976eba6a14c8a8e5dae64
Joshua Austin <83935880+joshaustindev@users.noreply.github.com>
2022-01-08 17:03:50 -0600

    Final project

--- Shared Wallet/SharedWallet.sol
+++ Shared Wallet/SharedWallet.sol
@@ -3,16 +3,23 @@
 pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 
-contract ExtendedOwnable is Ownable {
+contract ImmutablyOwnable is Ownable {
 
     function isOwner() public view virtual returns (bool) {
         return msg.sender == owner();
     }
 
+    function renounceOwnership() public view override onlyOwner {
+        revert("Can't remove ownership");
+    }
+
 }
 
-contract Allowance is ExtendedOwnable {
+contract Allowance is ImmutablyOwnable {
+
+    using SafeMath for uint;
 
     event AllowanceChanged(address indexed _forWhom, address indexed _fromWhom, uint _oldAmount, uint _newAmount);
 
@@ -29,13 +36,16 @@ contract Allowance is ExtendedOwnable {
     }
 
     function reduceAllowance(address _who, uint _amount) internal {
-        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
-        allowance[_who] -= _amount;
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who].sub(_amount));
+        allowance[_who] = allowance[_who].sub(_amount);
     }
 
 }
 
 contract SharedWallet is Allowance {
+
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
 
     function withdrawMoney(address payable _to, uint _amount) public ownerOrRecipient(_amount) {
         require(_amount <= address(this).balance, "There are not enough funds available");
@@ -45,7 +55,9 @@ contract SharedWallet is Allowance {
         _to.transfer(_amount);
     }
 
-    receive() external payable { }
+    receive() external payable {
+        emit MoneyReceived(msg.sender, msg.value);
+    }
 
-    fallback() external payable { }
+    fallback() external payable {}
 }
